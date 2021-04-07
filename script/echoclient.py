8f0a6bb07861758be5920fea85427996554da203
made9505 <81834259+made9505@users.noreply.github.com>
2021-04-07 13:23:35 -0500

    Update echoclient.py

--- script/echoclient.py
+++ script/echoclient.py
@@ -2,18 +2,14 @@ import socket
 import sys
 
 ser_port = 1234
-cli_port = 4321
 
 def client(ip):
     server_address = (ip, ser_port)
-    sock = socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP
-    )
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('', cli_port))
-    print('connecting to {}'.format(server_address))
 
     try:
+        print('connecting to {}'.format(server_address))
         sock.connect(server_address)
     except ConnectionRefusedError:
         print('connection to {} refused'.format(server_address))

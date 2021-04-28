02b697b3e7893d099af823542e5bcb0e29726cc3
Kirby Linvill <kjlinvill@gmail.com>
2021-04-28 16:15:13 -0700

    Refactor to use same proxy function for traffic both directions

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,9 @@
 //          - If data received from server, send to client using write() and then write_tls() (assuming rustls)
 //              - Optionally compress data before sending
 
+mod compression;
 mod forward_proxy;
 mod reverse_proxy;
-mod compression;
 
 use error_chain::bail;
 use error_chain::ChainedError;
